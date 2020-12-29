7a24dc0a6ae95911f35f38864ea5c357e0632e51
Vitor Argus <vitor.argus@outlook.com>
2020-12-29 16:43:46 -0300

    Upload inicial dos repositorios da agenda.

---
+++ argus-agenda-repositorios/src/br/argus/agenda/repositorios/impl/ContatoRepositorio.java
@@ -0,0 +1,34 @@
+package br.argus.agenda.repositorios.impl;
+
+import java.util.List;
+
+import br.argus.agenda.entidades.Contato;
+import br.argus.agenda.repositorios.interfaces.AgendaRepositorio;
+
+public class ContatoRepositorio implements AgendaRepositorio<Contato> {
+
+	@Override
+	public List<Contato> selecionar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserir(Contato entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Contato entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Contato entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
